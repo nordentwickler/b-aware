197905cc01cc75199a94bfe3aba1cd4f319a3bce
Marten Brosch <brosch@webhub.de>
2025-10-22 09:56:06 +0200

    fix

--- src/scripts/components/AccordionItem.vue
+++ src/scripts/components/AccordionItem.vue
@@ -9,7 +9,7 @@
           <span :class="summaryClasses">{{ title }}</span>
         </span>
 
-        <svg viewBox="0 0 20 24" class="h-6 w-6 transition-all duration-200" :class="showDetails ? 'rotate-180' : 'rotate-0'">
+        <svg viewBox="0 0 20 24" class="h-6 w-6 transition-all duration-200 shrink-0 " :class="showDetails ? 'rotate-180' : 'rotate-0'">
           <path d="m18.838 12.744-8.057 8.057V.78H9.22v20.02l-8.057-8.057-1.074 1.074L10 23.73l9.912-9.912z" fill="#D52E7F" fill-rule="nonzero"/>
         </svg>
 
